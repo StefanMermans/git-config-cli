d7aa1f26f17afd812097dbbaaaa79ecaab330f41
Stefan <stefanmermans99@gmail.com>
2023-11-12 12:31:22 +0100

    Improved error handling

--- src/git_config.rs
+++ src/git_config.rs
@@ -1,16 +1,15 @@
-use std::process::Command;
+use std::{process::Command, io::Error};
 
 use crate::Profile;
 
-pub fn set_local_profile(profile: &Profile) {
+pub fn set_local_profile(profile: &Profile) -> Result<(), Error> {
     let result = Command::new("git")
         .args([
             "config",
             "user.email",
             format!("{}", &profile.email).as_str(),
         ])
-        .output()
-        .expect("Failed to run command");
+        .output()?;
 
     if !result.status.success() {
         println!("Failed to set email!");
@@ -18,10 +17,11 @@ pub fn set_local_profile(profile: &Profile) {
 
     let result = Command::new("git")
         .args(["config", "user.name", format!("{}", &profile.name).as_str()])
-        .output()
-        .expect("Failed to run command");
+        .output()?;
 
     if !result.status.success() {
-        println!("Failed to set name!");
+        println!("Failed to set name");
     }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use crate::storage::Storage;
 use action::ActionType;
 use git_config::set_local_profile;
-use inquire::{Confirm, MultiSelect, Select, Text};
+use inquire::{Confirm, MultiSelect, Select, Text, InquireError};
 use profile::Profile;
 
 mod action;
@@ -11,12 +11,11 @@ mod git_config;
 mod profile;
 mod storage;
 
-fn create_credentials_actions(storage: &mut Storage) {
-    let name = Text::new("Enter a name").prompt().unwrap();
-    let email = Text::new("Enter an email").prompt().unwrap();
+fn create_credentials_actions(storage: &mut Storage) -> Result<(), InquireError> {
+    let name = Text::new("Enter a name").prompt()?;
+    let email = Text::new("Enter an email").prompt()?;
     let title = Text::new("What should the profile be called?")
-        .prompt()
-        .unwrap();
+        .prompt()?;
 
     let credentials = Profile { title, email, name };
 
@@ -25,54 +24,62 @@ fn create_credentials_actions(storage: &mut Storage) {
         &credentials.title, &credentials.name, &credentials.email
     );
 
-    let confirm = Confirm::new("Is this correct? (y/n)").prompt().unwrap();
+    let confirm = Confirm::new("Is this correct? (y/n)").prompt()?;
 
     if !confirm {
-        create_credentials_actions(storage);
-        return;
+        create_credentials_actions(storage)?;
+        return Ok(());
     }
 
     storage.store(credentials);
+
+    Ok(())
 }
 
-fn select_profile(storage: &Storage) {
+fn select_profile(storage: &Storage) -> Result<(), InquireError> {
     let profiles = storage.profiles();
 
     if profiles.len() == 0 {
         println!("No profiles available");
 
-        return;
+        return Ok(());
     }
 
     let selected = Select::new("What user do you want to use", storage.profiles())
-        .prompt()
-        .unwrap();
+        .prompt()?;
 
-    set_local_profile(&selected);
+    set_local_profile(&selected)?;
+
+    Ok(())
 }
 
-fn delete_profile(storage: &mut Storage) {
+fn delete_profile(storage: &mut Storage) -> Result<(), InquireError> {
     let selected = MultiSelect::new("Select profiles to delete", storage.profiles())
-        .prompt()
-        .unwrap();
+        .prompt()?;
 
     for profile in selected {
         storage.delete_profile(&profile);
     }
+
+    Ok(())
+}
+
+fn inquire(storage: &mut Storage) -> Result<(), InquireError> {
+    let action = Select::new("What do you want to do?", ActionType::as_vec())
+        .prompt()
+        .expect("Something went wrong");
+
+    match action {
+        ActionType::SelectProfile => select_profile(storage),
+        ActionType::CreateProfile => create_credentials_actions(storage),
+        ActionType::DeleteProfile => delete_profile(storage),
+    }
 }
 
 fn main() {
-    let home = env::var("HOME").unwrap();
+    let home = env::var("HOME").expect("Failed to get HOME dir");
     let storage_dir = format!("{}/.config/git-config-cli", home);
     let mut storage = Storage::new(storage_dir, "profiles.json".to_string());
 
-    let action = Select::new("What do you want to do?", ActionType::as_vec())
-        .prompt()
-        .unwrap();
-
-    match action {
-        ActionType::SelectProfile => select_profile(&storage),
-        ActionType::CreateProfile => create_credentials_actions(&mut storage),
-        ActionType::DeleteProfile => delete_profile(&mut storage),
-    }
+    inquire(&mut storage).expect("Something went wrong!");
 }
